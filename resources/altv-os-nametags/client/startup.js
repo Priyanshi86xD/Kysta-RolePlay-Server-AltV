6ff0e0f38641c6f51a20592c2310046c73407886
Frenzzys <122144999+Priyanshi86xD@users.noreply.github.com>
2023-06-23 07:48:49 +0530

    Add files via upload

---
+++ resources/altv-os-nametags/client/startup.js
@@ -0,0 +1,159 @@
+/// <reference types="@altv/types-client" />
+import alt from 'alt-client';
+import * as native from 'natives';
+
+let hideNametagsInVehicle = false;
+let showBarsOnAim = true;
+let drawDistance = 25;
+let showNametags = false;
+let interval;
+
+alt.onServer('nametags:Config', handleConfig);
+
+/**
+ * @param  {Boolean} _showNametags
+ * @param  {Boolean} _hideNamesInVehicles
+ * @param  {Boolean} _showBarsOnAim
+ * @param  {Number} _maxDrawDistance
+ */
+function handleConfig(_showNametags, _hideNamesInVehicles, _showBarsOnAim, _maxDrawDistance) {
+    showNametags = _showNametags;
+    hideNametagsInVehicle = _hideNamesInVehicles;
+    showBarsOnAim = _showBarsOnAim;
+    drawDistance = _maxDrawDistance;
+
+   // if (!showNametags) {
+      //  if (interval) {
+        //    alt.clearInterval(interval);
+         //   interval = null;
+      //  }
+      //  return;
+   // }
+
+   // interval = alt.setInterval(drawNametags, 0);
+}
+
+/**
+ * Toggled on through an interval.
+ */
+
+
+alt.on('shownametag', drawNametags)
+
+function drawNametags() {
+let draw = alt.setInterval(()=>{
+
+    if (alt.Player.local.getSyncedMeta('STOP_DRAWS')) {
+        return;
+    }
+
+    for (let i = 0, n = alt.Player.all.length; i < n; i++) {
+        let player = alt.Player.all[i];
+        if (!player.valid) {
+            continue;
+        }
+
+        if (hideNametagsInVehicle && player.vehicle && alt.Player.local.vehicle !== player.vehicle) {
+            continue;
+        }
+
+        if (player.scriptID === alt.Player.local.scriptID) {
+            continue;
+        }
+
+        const name = player.getSyncedMeta('NAME');
+        if (!name) {
+            continue;
+        }
+
+        if (!native.hasEntityClearLosToEntity(alt.Player.local.scriptID, player.scriptID, 17)) {
+            continue;
+        }
+
+        let dist = distance2d(player.pos, alt.Player.local.pos);
+        if (dist > drawDistance) {
+            continue;
+        }
+
+        const isChatting = player.getSyncedMeta('CHATTING');
+        const pos = { ...native.getPedBoneCoords(player.scriptID, 12844, 0, 0, 0) };
+        pos.z += 0.75;
+
+        let scale = 1 - (0.8 * dist) / drawDistance;
+        let fontSize = 0.6 * scale;
+
+        const lineHeight = fontSize / drawDistance;
+        const entity = player.vehicle ? player.vehicle.scriptID : player.scriptID;
+        const vector = native.getEntityVelocity(entity);
+        const frameTime = native.getFrameTime();
+
+        // Names
+        native.setDrawOrigin(
+            pos.x + vector.x * frameTime,
+            pos.y + vector.y * frameTime,
+            pos.z + vector.z * frameTime,
+            0
+        );
+        native.beginTextCommandDisplayText('STRING');
+        native.setTextFont(4);
+        native.setTextScale(fontSize, fontSize);
+        native.setTextProportional(true);
+        native.setTextCentre(true);
+        native.setTextColour(255, 255, 255, 255);
+        native.setTextOutline();
+        native.addTextComponentSubstringPlayerName(isChatting ? `${name}~r~*` : `${name}`);
+        native.endTextCommandDisplayText(0, 0, 0);
+
+        const [foundEntity, aimingAtPed] = native.getEntityPlayerIsFreeAimingAt(
+            alt.Player.local.scriptID,
+            player.scriptID
+        );
+
+        // Bars
+        if (showBarsOnAim && foundEntity && aimingAtPed === player.scriptID && !native.isEntityDead(player.scriptID,true)) {
+            drawBarBackground(100, lineHeight, scale, 0.85, 139, 0, 0, 255);
+            drawBar(native.getEntityHealth(player.scriptID) - 100, lineHeight, scale, 0.85, 255, 0, 0, 255);
+
+            if (native.getPedArmour(player.scriptID) > 0) {
+                drawBarBackground(100, lineHeight, scale, 1.10, 140, 140, 140, 255);
+                drawBar(native.getPedArmour(player.scriptID), lineHeight, scale, 1.10, 255, 255, 255, 255);
+            }
+        }
+
+        native.clearDrawOrigin();
+    }
+},10)
+let stop = alt.setInterval(()=>{
+    alt.clearInterval(draw);
+    alt.clearInterval(stop);
+}, 5000);
+}
+
+/**
+ * @param  {alt.Vector3} vector1
+ * @param  {alt.Vector3} vector2
+ */
+function distance2d(vector1, vector2) {
+    return Math.sqrt(Math.pow(vector1.x - vector2.x, 2) + Math.pow(vector1.y - vector2.y, 2));
+}
+
+function drawBar(value, lineHeight, scale, position, r, g, b, a) {
+    const healthWidth = value * 0.0005 * scale;
+    native.drawRect(
+        (healthWidth - 100 * 0.0005 * scale) / 2,
+        lineHeight + position * lineHeight,
+        healthWidth,
+        lineHeight / 4,
+        r,
+        g,
+        b,
+        a,
+        false
+    );
+}
+
+function drawBarBackground(value, lineHeight, scale, position, r, g, b, a) {
+    const width = value * 0.0005 * scale;
+    native.drawRect(0, lineHeight + position * lineHeight, width + 0.002, lineHeight / 3 + 0.002, 0, 0, 0, 255, false);
+    native.drawRect(0, lineHeight + position * lineHeight, width, lineHeight / 3, r, g, b, a, false);
+}
---
+++ resources/altv-os-nametags/server/startup.js
@@ -0,0 +1,28 @@
+/// <reference types="@altv/types-server" />
+import alt from 'alt-server';
+
+alt.on('nametags:Config', handleConfig);
+
+/**
+ * @param  {alt.Player} player
+ * @param  {Boolean} showNametags Draw nametags for all players for player?
+ * @param  {Boolean} hideNamesInVehicles=false
+ * @param  {Boolean} showBarsOnAim=false
+ * @param  {Number} maxDrawDistance=100
+ */
+
+function handleConfig(
+    player,
+    showNametags = true,
+    hideNamesInVehicles = false,
+    showBarsOnAim = true,
+    maxDrawDistance = 25
+) {
+    alt.emitClient(player, 'nametags:Config', showNametags, hideNamesInVehicles, showBarsOnAim, maxDrawDistance);
+}
+
+// Default display nametag as the name setup in altv, replace it if you want to display your own roleplay name
+alt.on('playerConnect', player => {
+    player.setSyncedMeta('NAME', player.name);
+    alt.emit('nametags:Config', player, true, false, true, 25);
+});
